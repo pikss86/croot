0b1081e7a8813bf7b10750a5f20b1887ec8bfae6
Ermolaev Pavel <Ermolaev_PV@surgutneftegas.ru>
2022-07-29 14:52:01 +0500

    описание задач

---
+++ drafts/upload/1_http_form/server.js
@@ -0,0 +1,11 @@
+﻿const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    console.log('request', req.url);
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    const indexfs = fs.createReadStream('index.html');
+    indexfs.pipe(res);
+});
+
+server.listen(3000, 'localhost', () => console.log('server started'));
